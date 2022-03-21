6b46e047ec96852cb499581d31f9f295d5651717
ImadAbid <imad.abied@gmail.com>
2022-03-21 17:16:13 +0100

    regisring users

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 
+const bcrypt = require('bcryptjs');
+
+const salt = bcrypt.genSaltSync(10);
 
 const bodyParser = require("body-parser");
-const { send } = require("process");
 app.use(bodyParser.urlencoded({ extended: false }));
 
 const MongoClient = require('mongodb').MongoClient;
@@ -14,15 +16,27 @@ const url = 'mongodb://localhost:27017';
 MongoClient.connect(url)
 // On commence par récupérer la collection que l'on va utiliser et la passer
 .then(function (client) {
-  console.log("ok");
-  return client.db("products_manager").collection("products");
+  return client.db("tinderb").collection("users");
 })
-.then((products) => {
+.then((users) => {
 
     // Rajouter vos routes et les traitements
 
     app.get('/welcome', (req, res)=>{
         res.send("Hello World");
+    });
+
+    app.post('/users/register', (req, res)=>{
+        client = {}
+
+        client.username = req.body.username;
+        client.email = req.body.email;
+        client.salt = 
+        client.hashed_password = bcrypt.hashSync(req.body.password, salt);
+
+        users.insertOne(client, (insertOne_err, insertOne_res)=>{
+            res.send("Client registerd !");
+        });
     });
 
     app.listen(3000, () => {
