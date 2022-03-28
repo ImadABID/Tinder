d5a77d5cd67f3e42c09208cd2c5f357cb193ac67
Ghariani Ayman <aghariani@bordeaux-inp.fr>
2022-03-28 16:31:55 +0200

    Login, Singup and Profile screens

---
+++ client/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import ProfilScreen from './screens/ProfilScreen';
+import LoginScreen from './screens/LoginScreen';
+import SingipScreen from './screens/SignupScreen';
+
+const AppSignUp = () => {
+  return (
+    <View>
+      
+      <LoginScreen/>
+
+      </View>
+  );
+}
+
+
+
+export default AppSignUp; 
