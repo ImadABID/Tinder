3c639d3b0b3fe0a3b9d99559870704c312869adf
GharianiAyman <aghariani001@bordeaux-inp.fr>
2022-04-25 22:30:59 +0200

    fix server response on localisation

--- client/screens/TinderCard.js
+++ client/screens/TinderCard.js
@@ -1,4 +1,4 @@
-import React , { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, ImageBackground } from 'react-native';
 import CardStack, { Card } from 'react-native-card-stack-swiper';
 import CardItem from '../components/CardItem';
@@ -25,106 +25,86 @@ const TinderCard = () => {
     latitude: 27.950575,
     longitude: -82.457178
   }
-  console.log( "la distance est = " + haversine(start, end, {unit: 'meter'}))
-  useEffect(() => {
-    (async () => {
+  console.log("la distance est = " + haversine(start, end, { unit: 'meter' }))
+
+
+  const at_start_up = async () => {
+
+    let token = await SecureStore.getItemAsync('token');
+    if (token) {
+      //
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== 'granted') {
         setErrorMsg('Permission to access location was denied');
         return;
       }
+      var location = await Location.getCurrentPositionAsync({});
+      let text = 'Waiting..';
+      if (errorMsg) {
+        text = errorMsg;
+      } else if (location) {
+        var latitude = location.coords.latitude;
+        var longitude = location.coords.longitude;
 
-      let location = await Location.getCurrentPositionAsync({});
-    })();
-  }, []);
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    var latitude = location.coords.latitude;
-    var longitude = location.coords.latitude;
 
-    
-  }
-  const at_start_up = async () => {
-        
-    let token = await SecureStore.getItemAsync('token');
-    if (token) {
-        
-        let host_name = await ip_server.get_hostname();
-        let link = 'http://'+host_name+'/users/profile';
+      }
+      //
+      let host_name = await ip_server.get_hostname();
 
-        let data = 'token='+token;
+      let data = 'token=' + token;
+      data = data + '&latitude=' + latitude + '&longitude=' + longitude;
+      let linkLoc = 'http://' + host_name + '/users/setLocalisation';
+      let reqLoc = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },// this line is important, if this content-type is not set it wont work
+        body: data
 
-        let myInit = {
-            method: 'POST',
-            headers: {'Content-Type':'application/x-www-form-urlencoded'}, // this line is important, if this content-type is not set it wont work
-            body: data
-        };
-
-        fetch(link, myInit)
-        .then((res)=>{return res.json();})
-        .then( res =>{
-
-          if(res.client.hasOwnProperty('email')){
-                let data = 'email='+res.client.email + '&latitude=' + latitude + '&longitude=' + longitude; 
-                let linkLoc = 'http://'+host_name+'/users/setLocalisation';
-                let reqLoc = {
-                  method: 'POST',
-                  headers: {'Content-Type':'application/x-www-form-urlencoded'},// this line is important, if this content-type is not set it wont work
-                  body: data             
-
-              };
-              fetch(linkLoc, reqLoc)
-              .then((resLoc)=>{return resLoc.json();})
-              .then( resLoc =>{        
-                console.log(resLoc)
-              }).catch(errloc => {
-                console.log("something wrong in Localisation")
-              });
-            }
-
+      };
+      fetch(linkLoc, reqLoc)
+        .then((res) => { return res.json(); })
+        .then(res => {
+          console.log(res)
         }).catch(err => {
           console.log(err)
         });
-        
-    }else{
-        navigation.navigate('LoginScreen');
+
+    } else {
+      navigation.navigate('LoginScreen');
     }
-}
+  }
   useFocusEffect(
     React.useCallback(() => {
-        at_start_up();
+      at_start_up();
     })
-);
+  );
   return (
 
-      <View style={styles.containerHome}>
-        <View style={styles.top}>
+    <View style={styles.containerHome}>
+      <View style={styles.top}>
 
-        </View>
-
-        <CardStack
-          loop={true}
-          verticalSwipe={false}
-          renderNoMoreCards={() => null}
-          ref={swiper => { t.swiper = swiper }}
-        >
-          {Demo.map((item, index) => (
-            <Card key={index}>
-              <CardItem
-                image={item.image}
-                name={item.name}
-                description={item.description}
-                matches={item.match}
-                actions
-                onPressLeft={() => t.swiper.swipeLeft()}
-                onPressRight={() => t.swiper.swipeRight()}
-              />
-            </Card>
-          ))}
-        </CardStack>
       </View>
+
+      <CardStack
+        loop={true}
+        verticalSwipe={false}
+        renderNoMoreCards={() => null}
+        ref={swiper => { t.swiper = swiper }}
+      >
+        {Demo.map((item, index) => (
+          <Card key={index}>
+            <CardItem
+              image={item.image}
+              name={item.name}
+              description={item.description}
+              matches={item.match}
+              actions
+              onPressLeft={() => t.swiper.swipeLeft()}
+              onPressRight={() => t.swiper.swipeRight()}
+            />
+          </Card>
+        ))}
+      </CardStack>
+    </View>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -10,11 +10,11 @@ var token_sig = 'pjezfpjajfajeipjfez4845as5';
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
+const cors = require("cors");
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
 }
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
@@ -24,194 +24,215 @@ const url = 'mongodb://localhost:27017';
 
 // On ouvre une connexion à notre base de données
 MongoClient.connect(url)
-// On commence par récupérer la collection que l'on va utiliser et la passer
-.then(function (client) {
-  return client.db("tinderb").collection("users");
-})
-.then((users) => {
-
-  // Rajouter vos routes et les traitements
-
-  // just for test in dev mode
-  app.get('/welcome', (req, res)=>{
-    console.log('welcome')
-    res.json({respond:"Hello_World"});
-  });
-
-
-  app.post('/users/setLocalisation', (req, res)=>{
-    //console.log(req.body.email)
-    users.updateOne(
-      { "email": req.body.email}, // Filter
-      {$set:{"latitude": req.body.latitude,
-      "longitude": req.body.longitude } },// Update
-  )
-  .then((obj) => {
-      console.log('Updated - ' + obj);
-
-    })
-  .catch((err) => {
-      console.log('Error: ' + err);
+  // On commence par récupérer la collection que l'on va utiliser et la passer
+  .then(function (client) {
+    return client.db("tinderb").collection("users");
   })
-  });
-  /*app.post('/users/setLocalisation', (req, res)=>{
-    //console.log(req.body.email)
-    users.find({}).toArray(function(err, result) {
-      if (err) throw err;
-      console.log(result);
+  .then((users) => {
+
+    // Rajouter vos routes et les traitements
+
+    // just for test in dev mode
+    app.get('/welcome', (req, res) => {
+      console.log('welcome')
+      res.json({ respond: "Hello_World" });
     });
-  
-  });
-*/
-    
 
-  app.post('/users/register', (req, res)=>{
 
-    /*
-      curl
-        -X POST 
-        -d 'username=lora'
-        -d 'email=lora17@yml.fr'
-        -d 'password=kona75mi:-)'
-        http://localhost:3000/users/register
-    */
+    app.post('/users/setLocalisation', (req, res) => {
 
-    client = {}
 
-    client.username = req.body.username;
-    client.email = req.body.email;
-    let salt = bcrypt.genSaltSync(10);
-    client.hashed_password = bcrypt.hashSync(req.body.password, salt);
-
-    // verify email uniqueness
-    users.findOne(
-      {"email" : req.body.email},
-      (err, cl)=>{
-        if(cl){
-          res.json({msg:'email already used'});
-        }else{
-
-          users.insertOne(client, (insertOne_err, insertOne_res)=>{
-            if(insertOne_err){
-              console.log(insertOne_err);
-              res.json({msg:'Uknown problem'});
-            }else{
-
-              let token_ele = {
-                email : client.email
+      /*jwt.verify(req.body.token, token_sig, (err, user)=>{
+        
+        users.findOne(
+          {"email" : user.email},
+          (err, client)=>{
+            res.json(
+              {
+                msg:'0',
+                client : client
               }
-              let token = jwt.sign(token_ele, token_sig);
-              res.json({msg:'0', token:token});
-
-            }
-          });
-
-        }
-      }
-    )
-  
-  });
-
-
-  app.post('/users/login', (req, res)=>{
-    
-    /*
-      curl
-        -X POST 
-        -d 'email=lora17@yml.fr'
-        -d 'password=kona75mi:-)'
-        http://localhost:3000/users/login
-    */
-
-    console.log('login');
-
-    users.findOne(
-      {"email" : req.body.email},
-      (err, client)=>{
-        if(client == null){
-          res.json({msg:'not registred'});
-        }else{
-
-          if(bcrypt.compareSync(req.body.password, client.hashed_password)){
-          
-            let token_ele = {
-              email : client.email
-            }
-            let token = jwt.sign(token_ele, token_sig);
-            res.json({msg:'0', token:token});
-            
-          }else{
-            res.json({msg:'wrong password'});
+            );
           }
-
-        }
-      }
-    )
-
-  });
-
-  // just for test in dev mode
-  app.post('/users/is_registered', (req, res)=>{
-    
-    /*
-      curl
-        -X POST 
-        -d 'email=lora17@yml.fr'
-        -d 'password=kona75mi:-)'
-        http://localhost:3000/users/is_registered
-    */
-
-    users.findOne(
-      {"email" : req.body.email},
-      (err, client)=>{
-        if(bcrypt.compareSync(req.body.password, client.hashed_password)){
-          res.send("registerd");
-        }else{
-          res.send("not_registerd");
-        }
-      }
-    )
-
-  });
-
-  app.post('/users/profile', (req, res)=>{
-    
-    /*
-      curl
-        -X POST 
-        -d 'token=lora17@yml.fr'
-        http://localhost:3000/users/profile
-    */
-
-    jwt.verify(req.body.token, token_sig, (err, user)=>{
-      
-      users.findOne(
-        {"email" : user.email},
-        (err, client)=>{
-          res.json(
-            {
-              msg:'0',
-              client : client
+        )
+        
+      })
+      */
+      jwt.verify(req.body.token, token_sig, (err, user) => {
+        users.updateOne(
+          { "email": user.email }, // Filter
+          {
+            $set: {
+              "latitude": req.body.latitude,
+              "longitude": req.body.longitude
             }
-          );
+          },// Update
+        )
+          .then((obj) => {
+            res.json({ msg: 'updated' })
+          })
+          .catch((err) => {
+            console.log('Error: ' + err);
+          })
+      })
+    });
+    /*
+    app.post('/users/setLocalisation', (req, res) => {
+      users.find({}).toArray(function (err, result) {
+          console.log(result);
+      });
+
+    });*/
+
+
+
+    app.post('/users/register', (req, res) => {
+
+      /*
+        curl
+          -X POST 
+          -d 'username=lora'
+          -d 'email=lora17@yml.fr'
+          -d 'password=kona75mi:-)'
+          http://localhost:3000/users/register
+      */
+
+      client = {}
+
+      client.username = req.body.username;
+      client.email = req.body.email;
+      let salt = bcrypt.genSaltSync(10);
+      client.hashed_password = bcrypt.hashSync(req.body.password, salt);
+
+      // verify email uniqueness
+      users.findOne(
+        { "email": req.body.email },
+        (err, cl) => {
+          if (cl) {
+            res.json({ msg: 'email already used' });
+          } else {
+
+            users.insertOne(client, (insertOne_err, insertOne_res) => {
+              if (insertOne_err) {
+                console.log(insertOne_err);
+                res.json({ msg: 'Uknown problem' });
+              } else {
+
+                let token_ele = {
+                  email: client.email
+                }
+                let token = jwt.sign(token_ele, token_sig);
+                res.json({ msg: '0', token: token });
+
+              }
+            });
+
+          }
         }
       )
-      
-    })
+
+    });
 
 
+    app.post('/users/login', (req, res) => {
+
+      /*
+        curl
+          -X POST 
+          -d 'email=lora17@yml.fr'
+          -d 'password=kona75mi:-)'
+          http://localhost:3000/users/login
+      */
+
+      console.log('login');
+
+      users.findOne(
+        { "email": req.body.email },
+        (err, client) => {
+          if (client == null) {
+            res.json({ msg: 'not registred' });
+          } else {
+
+            if (bcrypt.compareSync(req.body.password, client.hashed_password)) {
+
+              let token_ele = {
+                email: client.email
+              }
+              let token = jwt.sign(token_ele, token_sig);
+              res.json({ msg: '0', token: token });
+
+            } else {
+              res.json({ msg: 'wrong password' });
+            }
+
+          }
+        }
+      )
+
+    });
+
+    // just for test in dev mode
+    app.post('/users/is_registered', (req, res) => {
+
+      /*
+        curl
+          -X POST 
+          -d 'email=lora17@yml.fr'
+          -d 'password=kona75mi:-)'
+          http://localhost:3000/users/is_registered
+      */
+
+      users.findOne(
+        { "email": req.body.email },
+        (err, client) => {
+          if (bcrypt.compareSync(req.body.password, client.hashed_password)) {
+            res.send("registerd");
+          } else {
+            res.send("not_registerd");
+          }
+        }
+      )
+
+    });
+
+    app.post('/users/profile', (req, res) => {
+
+      /*
+        curl
+          -X POST 
+          -d 'token=lora17@yml.fr'
+          http://localhost:3000/users/profile
+      */
+
+      jwt.verify(req.body.token, token_sig, (err, user) => {
+
+        users.findOne(
+          { "email": user.email },
+          (err, client) => {
+            res.json(
+              {
+                msg: '0',
+                client: client
+              }
+            );
+          }
+        )
+
+      })
+
+
+    });
+
+    // just for test in dev mode
+    app.get('/drop_db', (req, res) => {
+      users.drop();
+      res.send("drop_db");
+    });
+
+    app.listen(3000, () => {
+      console.log("En attente de requêtes...");
+    });
+  })
+  .catch(function (err) {
+    throw err;
   });
-
-  // just for test in dev mode
-  app.get('/drop_db', (req, res) => {
-    users.drop();
-    res.send("drop_db");
-  });
-
-  app.listen(3000, () => {
-    console.log("En attente de requêtes...");
-  });
-})
-.catch(function (err) {
-  throw err;
-});
