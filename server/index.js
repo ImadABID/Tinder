f915afd4caa1acb4b0b6fa83e21ae63e28edefd6
ImadAbid <imad.abied@gmail.com>
2022-04-29 17:13:56 +0200

    server responding to visited profile

--- server/index.js
+++ server/index.js
@@ -43,274 +43,237 @@ const url = 'mongodb://localhost:27017';
 
 // On ouvre une connexion à notre base de données
 MongoClient.connect(url)
-  // On commence par récupérer la collection que l'on va utiliser et la passer
-  .then(function (client) {
-    return client.db("tinderb");
-  })
-  .then((db) => {
-    
-    // Rajouter vos routes et les traitements
+// On commence par récupérer la collection que l'on va utiliser et la passer
+.then(function (client) {
+  return client.db("tinderb");
+})
+.then((db) => {
+  
+  // Rajouter vos routes et les traitements
 
-    // just for test in dev mode
-    app.get('/welcome', (req, res) => {
-      console.log('welcome')
-      res.json({ respond: "Hello_World" });
-    });
-
-
-    app.post('/matches/get', (req, res) => {
-
-      /*
-        curl
-          -X POST 
-          -d 'token=lora17@yml.fr'
-          http://localhost:3000/users/profile
-      */
+  // just for test in dev mode
+  app.get('/welcome', (req, res) => {
+    console.log('welcome')
+    res.json({ respond: "Hello_World" });
+  });
 
 
-      jwt.verify(req.body.token, token_sig, (err, user)=>{
-        
-        db.collection("users").find({}).toArray(function (err, matches) {
-          db.collection("matches").find({ }).toArray(function (err, matcher) {
+  app.post('/matches/get', (req, res) => {
 
-          let data = [{}];
-          i = 0;
-          for (let attr in matches) {
-            let test = 0 ; 
-            for (let ind in matcher) {
-              if( ( matcher[ind].action == "yes" ) && ( (matches[attr].email == matcher[ind].email_1) || (matches[attr].email == matcher[ind].email_2) ) &&
-               ( (user.email == matcher[ind].email_1) || (user.email == matcher[ind].email_2) ) )
-              {                    
-                test = 1 ; 
-              }
-            }                  
-              if ( (test == 1 ) ) {
-
-              data[i] = Object.assign(matches[attr]);
-              i++;
-            }
-            
-          }
-          const jsonAsArray = Object.keys(data).map(function (key) {
-            return data[key];
-          })
-            .sort(function (itemA, itemB) {
-              return itemA._id < itemB._id;
-            });
-          res.json({ jsonAsArray })
-        });
-        });
-        
-      })
-
-
-
-    });
-    app.post('/matches/set', (req, res) => {
-
-      /*
-        curl
-          -X POST 
-          -d 'token=lora17@yml.fr'
-          http://localhost:3000/users/profile
-      */
-
-      /*jwt.verify(req.body.token, token_sig, (err, user)=>{
-        
-        db.collection("matches").findOne(
-          {"email_1" : user.email},
-          (err, client)=>{
-            console.log(client)
-          }
-        )
-        
-      })*/
-      
-      jwt.verify(req.body.token, token_sig, (err, user)=>{
-
-        var myobj = { 'email_1' : user.email , 'email_2' : req.body.email, 'action' : req.body.action  };
-        db.collection("matches").insertOne(myobj, function(err, res) {
-          if (err) throw err;
-          console.log("1 element inserted");
-
-        });
-        
-      })
-
-
-
-    });
-    app.post('/users/setLocalisation', (req, res) => {
-      
-      function deg2rad(deg) {
-        return deg * (Math.PI / 180)
-      }
-      function getDistance(lat1, lon1, lat2, lon2) {
-        var R = 6371; // Radius of the earth in km
-        var dLat = deg2rad(lat2 - lat1);  // deg2rad below
-        var dLon = deg2rad(lon2 - lon1);
-        var a =
-          Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-          Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-          Math.sin(dLon / 2) * Math.sin(dLon / 2)
-          ;
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-        var d = R * c; // Distance in km
-        return d;
-      }
-
-      /*jwt.verify(req.body.token, token_sig, (err, user)=>{
-        
-        users.findOne(
-          {"email" : user.email},
-          (err, client)=>{
-            res.json(
-              {
-                msg:'0',
-                client : client
-              }
-            );
-          }
-        )
-        
-      })
-      */
-      jwt.verify(req.body.token, token_sig, (err, user) => {
-
-        db.collection("users").updateOne(
-          { "email": user.email }, // Filter
-          {
-            $set: {
-              "latitude": req.body.latitude,
-              "longitude": req.body.longitude
-            }
-          },// Update
-        )
-          
-          .then((obj) => {
-        
-
-
-            //db.collection("matches").deleteMany({});
-            db.collection("users").find({}).toArray(function (err, matches) {
-              db.collection("matches").find({ }).toArray(function (err, matcher) {
-
-              let data = {};
-              let tmp;
-              let dist;
-              i = 0;
-              for (let attr in matches) {
-                let test = 0 ; 
-                for (let ind in matcher) {
-                  if( ( (matches[attr].email == matcher[ind].email_1) || (matches[attr].email == matcher[ind].email_2) ) &&
-                   ( (user.email == matcher[ind].email_1) || (user.email == matcher[ind].email_2) ) )
-                  {                    
-                    test = 1 ; 
-                  }
-                }                  
-                  if ((getDistance(req.body.latitude, req.body.longitude, matches[attr].latitude, matches[attr].longitude) < 10) && (test == 0 ) && (i<20) ) {
-                  dist = { distance: getDistance(req.body.latitude, req.body.longitude, matches[attr].latitude, matches[attr].longitude) }
-                  tmp = Object.assign(matches[attr], dist);
-                  data[i] = tmp;
-                  i++;
-                }
-                
-              }
-
-              const jsonAsArray = Object.keys(data).map(function (key) {
-                return data[key];
-              })
-                .sort(function (itemA, itemB) {
-                  return itemA.distance < itemB.distance;
-                });
-              res.json({ jsonAsArray })
-            });
-            });
-
-          })
-          .catch((err) => {
-            console.log('Error: ' + err);
-          })
-      })
-    });
     /*
-    app.post('/users/setLocalisation', (req, res) => {
-      users.find({}).toArray(function (err, result) {
-          console.log(result);
-      });
-
-    });*/
-
+      curl
+        -X POST 
+        -d 'token=lora17@yml.fr'
+        http://localhost:3000/users/profile
+    */
 
 
-    app.post('/users/register', (req, res) => {
+    jwt.verify(req.body.token, token_sig, (err, user)=>{
+      
+      db.collection("users").find({}).toArray(function (err, matches) {
+        db.collection("matches").find({ }).toArray(function (err, matcher) {
 
-      /*
-        curl
-          -X POST 
-          -d 'username=lora'
-          -d 'email=lora17@yml.fr'
-          -d 'password=kona75mi:-)'
-          http://localhost:3000/users/register
-      */
+        let data = [{}];
+        i = 0;
+        for (let attr in matches) {
+          let test = 0 ; 
+          for (let ind in matcher) {
+            if( ( matcher[ind].action == "yes" ) && ( (matches[attr].email == matcher[ind].email_1) || (matches[attr].email == matcher[ind].email_2) ) &&
+              ( (user.email == matcher[ind].email_1) || (user.email == matcher[ind].email_2) ) )
+            {                    
+              test = 1 ; 
+            }
+          }                  
+            if ( (test == 1 ) ) {
 
-      client = {}
-
-      client.username = req.body.username;
-      client.email = req.body.email;
-      let salt = bcrypt.genSaltSync(10);
-      client.hashed_password = bcrypt.hashSync(req.body.password, salt);
-
-      // verify email uniqueness
-      db.collection("users").findOne(
-        { "email": req.body.email },
-        (err, cl) => {
-          if (cl) {
-            res.json({ msg: 'email already used' });
-          } else {
-
-            db.collection("users").insertOne(client, (insertOne_err, insertOne_res) => {
-              if (insertOne_err) {
-                console.log(insertOne_err);
-                res.json({ msg: 'Uknown problem' });
-              } else {
-
-                let token_ele = {
-                  email: client.email
-                }
-                let token = jwt.sign(token_ele, token_sig);
-                res.json({ msg: '0', token: token });
-
-              }
-            });
-
+            data[i] = Object.assign(matches[attr]);
+            i++;
           }
+          
+        }
+        const jsonAsArray = Object.keys(data).map(function (key) {
+          return data[key];
+        })
+          .sort(function (itemA, itemB) {
+            return itemA._id < itemB._id;
+          });
+        res.json({ jsonAsArray })
+      });
+      });
+      
+    })
+
+
+
+  });
+  app.post('/matches/set', (req, res) => {
+
+    /*
+      curl
+        -X POST 
+        -d 'token=lora17@yml.fr'
+        http://localhost:3000/users/profile
+    */
+
+    /*jwt.verify(req.body.token, token_sig, (err, user)=>{
+      
+      db.collection("matches").findOne(
+        {"email_1" : user.email},
+        (err, client)=>{
+          console.log(client)
         }
       )
+      
+    })*/
+    
+    jwt.verify(req.body.token, token_sig, (err, user)=>{
 
+      var myobj = { 'email_1' : user.email , 'email_2' : req.body.email, 'action' : req.body.action  };
+      db.collection("matches").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 element inserted");
+
+      });
+      
+    })
+
+
+
+  });
+  app.post('/users/setLocalisation', (req, res) => {
+    
+    function deg2rad(deg) {
+      return deg * (Math.PI / 180)
+    }
+    function getDistance(lat1, lon1, lat2, lon2) {
+      var R = 6371; // Radius of the earth in km
+      var dLat = deg2rad(lat2 - lat1);  // deg2rad below
+      var dLon = deg2rad(lon2 - lon1);
+      var a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+        Math.sin(dLon / 2) * Math.sin(dLon / 2)
+        ;
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      var d = R * c; // Distance in km
+      return d;
+    }
+
+    /*jwt.verify(req.body.token, token_sig, (err, user)=>{
+      
+      users.findOne(
+        {"email" : user.email},
+        (err, client)=>{
+          res.json(
+            {
+              msg:'0',
+              client : client
+            }
+          );
+        }
+      )
+      
+    })
+    */
+    jwt.verify(req.body.token, token_sig, (err, user) => {
+
+      db.collection("users").updateOne(
+        { "email": user.email }, // Filter
+        {
+          $set: {
+            "latitude": req.body.latitude,
+            "longitude": req.body.longitude
+          }
+        },// Update
+      )
+        
+        .then((obj) => {
+      
+
+
+          //db.collection("matches").deleteMany({});
+          db.collection("users").find({}).toArray(function (err, matches) {
+            db.collection("matches").find({ }).toArray(function (err, matcher) {
+
+            let data = {};
+            let tmp;
+            let dist;
+            i = 0;
+            for (let attr in matches) {
+              let test = 0 ; 
+              for (let ind in matcher) {
+                if( ( (matches[attr].email == matcher[ind].email_1) || (matches[attr].email == matcher[ind].email_2) ) &&
+                  ( (user.email == matcher[ind].email_1) || (user.email == matcher[ind].email_2) ) )
+                {                    
+                  test = 1 ; 
+                }
+              }                  
+                if ((getDistance(req.body.latitude, req.body.longitude, matches[attr].latitude, matches[attr].longitude) < 10) && (test == 0 ) && (i<20) ) {
+                dist = { distance: getDistance(req.body.latitude, req.body.longitude, matches[attr].latitude, matches[attr].longitude) }
+                tmp = Object.assign(matches[attr], dist);
+                data[i] = tmp;
+                i++;
+              }
+              
+            }
+
+            const jsonAsArray = Object.keys(data).map(function (key) {
+              return data[key];
+            })
+              .sort(function (itemA, itemB) {
+                return itemA.distance < itemB.distance;
+              });
+            res.json({ jsonAsArray })
+          });
+          });
+
+        })
+        .catch((err) => {
+          console.log('Error: ' + err);
+        })
+    })
+  });
+  /*
+  app.post('/users/setLocalisation', (req, res) => {
+    users.find({}).toArray(function (err, result) {
+        console.log(result);
     });
 
+  });*/
 
-    app.post('/users/login', (req, res) => {
 
-      /*
-        curl
-          -X POST 
-          -d 'email=lora17@yml.fr'
-          -d 'password=kona75mi:-)'
-          http://localhost:3000/users/login
-      */
 
-      console.log('login');
+  app.post('/users/register', (req, res) => {
 
-      db.collection("users").findOne(
-        { "email": req.body.email },
-        (err, client) => {
-          if (client == null) {
-            res.json({ msg: 'not registred' });
-          } else {
+    /*
+      curl
+        -X POST 
+        -d 'username=lora'
+        -d 'email=lora17@yml.fr'
+        -d 'password=kona75mi:-)'
+        http://localhost:3000/users/register
+    */
 
-            if (bcrypt.compareSync(req.body.password, client.hashed_password)) {
+    client = {}
+
+    client.username = req.body.username;
+    client.email = req.body.email;
+    let salt = bcrypt.genSaltSync(10);
+    client.hashed_password = bcrypt.hashSync(req.body.password, salt);
+
+    // verify email uniqueness
+    db.collection("users").findOne(
+      { "email": req.body.email },
+      (err, cl) => {
+        if (cl) {
+          res.json({ msg: 'email already used' });
+        } else {
+
+          db.collection("users").insertOne(client, (insertOne_err, insertOne_res) => {
+            if (insertOne_err) {
+              console.log(insertOne_err);
+              res.json({ msg: 'Uknown problem' });
+            } else {
 
               let token_ele = {
                 email: client.email
@@ -318,64 +281,128 @@ MongoClient.connect(url)
               let token = jwt.sign(token_ele, token_sig);
               res.json({ msg: '0', token: token });
 
-            } else {
-              res.json({ msg: 'wrong password' });
             }
+          });
 
-          }
         }
-      )
+      }
+    )
 
-    });
+  });
 
-    // just for test in dev mode
-    app.post('/users/is_registered', (req, res) => {
 
-      /*
-        curl
-          -X POST 
-          -d 'email=lora17@yml.fr'
-          -d 'password=kona75mi:-)'
-          http://localhost:3000/users/is_registered
-      */
+  app.post('/users/login', (req, res) => {
 
-          db.collection("users").findOne(
-        { "email": req.body.email },
-        (err, client) => {
+    /*
+      curl
+        -X POST 
+        -d 'email=lora17@yml.fr'
+        -d 'password=kona75mi:-)'
+        http://localhost:3000/users/login
+    */
+
+    console.log('login');
+
+    db.collection("users").findOne(
+      { "email": req.body.email },
+      (err, client) => {
+        if (client == null) {
+          res.json({ msg: 'not registred' });
+        } else {
+
           if (bcrypt.compareSync(req.body.password, client.hashed_password)) {
-            res.send("registerd");
+
+            let token_ele = {
+              email: client.email
+            }
+            let token = jwt.sign(token_ele, token_sig);
+            res.json({ msg: '0', token: token });
+
           } else {
-            res.send("not_registerd");
+            res.json({ msg: 'wrong password' });
           }
+
         }
-      )
+      }
+    )
 
-    });
+  });
 
-    app.post('/users/profile', (req, res) => {
+  // just for test in dev mode
+  app.post('/users/is_registered', (req, res) => {
 
-      /*
-        curl
-          -X POST 
-          -d 'token=lora17@yml.fr'
-          http://localhost:3000/users/profile
-      */
-
-      jwt.verify(req.body.token, token_sig, (err, user) => {
+    /*
+      curl
+        -X POST 
+        -d 'email=lora17@yml.fr'
+        -d 'password=kona75mi:-)'
+        http://localhost:3000/users/is_registered
+    */
 
         db.collection("users").findOne(
-          { "email": user.email },
-          (err, client) => {
-            res.json(
-              {
-                msg: '0',
-                client: client
-              }
-            );
-          }
-        )
+      { "email": req.body.email },
+      (err, client) => {
+        if (bcrypt.compareSync(req.body.password, client.hashed_password)) {
+          res.send("registerd");
+        } else {
+          res.send("not_registerd");
+        }
+      }
+    )
 
-      })
+  });
+
+  app.post('/users/profile', (req, res) => {
+
+    /*
+      curl
+        -X POST 
+        -d 'token=lora17@yml.fr'
+        http://localhost:3000/users/profile
+    */
+
+    jwt.verify(req.body.token, token_sig, (err, user) => {
+
+      db.collection("users").findOne(
+        { "email": user.email },
+        (err, client) => {
+          res.json(
+            {
+              msg: '0',
+              client: client
+            }
+          );
+        }
+      )
+
+    })
+
+  });
+
+  app.post('/users/profile/visit', (req, res) => {
+
+    /*
+      curl
+        -X POST 
+        -d 'token=lora17@yml.fr'
+        http://localhost:3000/users/profile
+    */
+
+    jwt.verify(req.body.token, token_sig, (err, user) => {
+
+      db.collection("users").findOne(
+        { "email": req.body.email },
+        (err, client) => {
+          res.json(
+            {
+              msg: '0',
+              client: client
+            }
+          );
+        }
+      )
+
+    })
 
   });
 
@@ -542,7 +569,7 @@ MongoClient.connect(url)
 
     })
 
-    });
+  });
 
   app.get('/get_image', (req, res)=>{
     res.sendFile('uploads/'+req.query.filename , {root : __dirname});
